9ad9cb4b0b9b8ef7a2f16dda97c85ab872dba773
albertzzy <zhaozi_ye_hi@126.com>
2017-07-13 23:35:52 +0800

    add a schema

---
+++ schemas/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Mixed = Schema.Types.mixed;
+
+const user = new Schema({
+    name:{type:String},
+    groups:{type:String},
+    projects:{type:String},
+    password:{type:String},
+    email:{type:String}    
+})
+
+// middlewares
+
+module.exports = user;
