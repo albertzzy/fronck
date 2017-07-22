ec812722fbc7d355774207d91ae7bb84dd8ef1af
albertzzy <zhaozi_ye_hi@126.com>
2017-07-22 22:46:20 +0800

    add some files

---
+++ store/index.js
@@ -0,0 +1,30 @@
+const User = require('../model/user');
+
+
+
+class MgStore{
+
+    constructor(){
+
+
+    }
+
+
+    async get(key,maxAge,{rolling}){
+
+
+    }
+
+    async set(key,sess,maxAge,{rolling,changed}){
+        
+
+    }
+
+    async destory(key){
+
+    }
+
+
+}
+
+module.exports = MgStore;
