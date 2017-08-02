6fcfb9bb1d78a75fe8097f7d305e77dc51a696e6
albertzzy <zhaozi_ye_hi@126.com>
2017-08-02 17:51:36 +0800

    add webpack config file

--- statics/index.js
+++ statics/index.js
@@ -1,11 +1,9 @@
-import {observable, autorun} from 'mobx';
+import {observable, computed,autorun} from 'mobx';
 
 
-let todoStore = observable({
-    /* 一些观察的状态 */
+/*let todoStore = observable({
     todos: [],
 
-    /* 推导值 */
     get completedCount() {
         return this.todos.filter(todo => todo.completed).length;
     }
@@ -21,7 +19,35 @@ autorun(function() {
 });
 
 
+
+
 todoStore.todos[0] = {
     title: "Take a walk",
     completed: false
 };
+
+todoStore.todos[0].completed = true;*/
+
+
+/*const map = observable.map({ key: "value"});
+map.set("key", "new value");
+
+
+autorun(function(){
+    console.log(map.get('key'));
+
+})*/
+
+
+class OrderLine {
+    @observable price = 0;
+    @observable amount = 1;
+
+    @computed get total() {
+        return this.price * this.amount;
+    }
+}
+
+const line = new OrderLine();
+console.log("price" in line); // true
+console.log(line.hasOwnProperty("price")); // true, 现在所有的属性都定义在实例上了
